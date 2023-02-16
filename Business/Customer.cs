9a72610c7c3aa66c68d994c16028b07ba4b7e299
Mikkaiser <mikaelrsimoes19@gmail.com>
2023-02-15 23:18:42 -0300

    add: initial commit

---
+++ Business/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace Business;
+public class Customer
+{
+    public string Name { get; set; }
+    public List<Customer> Customers(string customerReceived)
+    {
+        //new Database.Customer.Customers();
+        List<Customer> customers = new();
+
+        customers.Add(new Customer() { Name = "Mikkaiser" });
+
+        return customers;
+    }
+}
---
+++ Database/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace Database;
+public class Customer
+{
+    public string Name { get; set; }
+    public List<Customer> Customers(string customerReceived)
+    {
+        
+        List<Customer> customers = new();
+
+        customers.Add(new Customer() { Name = "Mikkaiser"});
+
+        return customers;
+    }
+}
