3d95e612b29af3eec5b872b4e2154a2c49f8a0ed
Mikkaiser <mikaelrsimoes19@gmail.com>
2023-02-19 15:53:17 -0300

    add: connection string configuration

---
+++ Database/Page.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database;
+public class Page
+{
+    private readonly IConfiguration _configuration;
+
+    public Page(IConfiguration _configuration)
+    {
+        this._configuration = _configuration;
+    }
+    private string GetDatabaseConnection()
+    {
+        return _configuration.GetConnectionString("SqlServerConnectionString");
+    }
+
+    public DataTable GetPagesList()
+    {
+        SqlConnection sqlConnection = new(GetDatabaseConnection());
+    }
+}
